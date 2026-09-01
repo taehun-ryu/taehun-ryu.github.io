b78a0710e448046208471bf218f98c6481e7519a
taehun-ryu <xogns2079@gmail.com>
2026-09-01 18:31:04 +0900

    fix: exact hyper link

--- src/data/affiliations.ts
+++ src/data/affiliations.ts
@@ -1,12 +1,12 @@
 export const affiliations = [
   {
     name: 'Hanyang University',
-    href: 'https://www.hanyang.ac.kr/web/eng/identity',
+    href: 'https://www.hanyang.ac.kr/web/eng/erica-campus1',
     logo: '/images/affiliations/hanyang-university.svg',
   },
   {
     name: 'UNIST',
-    href: 'https://www.unist.ac.kr/unist/introduction/ui.do',
+    href: 'https://aigs.unist.ac.kr/eng/index.php',
     logo: '/images/affiliations/unist-wordmark.png',
   },
 ] as const;
